f5fd00ad7583892dc6610ec7284a2b94b75d830a
Cristian <cdnadaya@gmail.com>
2020-11-25 17:41:48 -0300

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,24 @@
 //      Puerto
 // =================
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ================= 
+//      Entorno
+// =================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'desa'
+
+// ================= 
+//   Base de Datos
+// =================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'desa') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://cdncor:Wp0n5j6Ro5XyAx0x@cluster0.gkaxx.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
